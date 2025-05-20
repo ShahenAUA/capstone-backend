745009fbf293324049ac5f4429a2a0d5800ff0df
Shahen <shahen_hovakimyan@edu.aua.am>
2025-05-20 17:46:46 +0400

    Implement delete listing functionality

--- mobile_api/views/authenticated/__init__.py
+++ mobile_api/views/authenticated/__init__.py
@@ -1,3 +1,4 @@
 from .user_me import UserGetMeView
-from .listing import (AddAdoptionListingView, AddLostListingView, GetMyAdoptionListingsView, GetMyLostListingsView)
+from .listing import (AddAdoptionListingView, AddLostListingView, GetMyAdoptionListingsView, GetMyLostListingsView,
+                      DeleteListingView)
 from .bookmark import AddListingBookmarkView, RemoveListingBookmarkView, GetBookmarkedListingsView
