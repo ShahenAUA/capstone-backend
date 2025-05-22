ae8fffc043fd31c7637fb940411f3f3afac66c9e
Shahen <shahen_hovakimyan@edu.aua.am>
2025-05-22 23:55:55 +0400

    Add bookmark information on listing details serializer

--- mobile_api/views/guest_listing.py
+++ mobile_api/views/guest_listing.py
@@ -170,7 +170,7 @@ class GetAdoptionListingDetailsView(generics.RetrieveAPIView):
     serializer_class = GetAdoptionListingDetailsSerializer
     permission_classes = [AllowAny,]
     lookup_field = 'id'
-    authentication_classes=[]
+    # authentication_classes=[]
 
     def get_queryset(self):
         return Listing.objects.filter(listing_type=Listing.ADOPTION)
@@ -192,7 +192,7 @@ class GetLostListingDetailsView(generics.RetrieveAPIView):
     serializer_class = GetLostListingDetailsSerializer
     permission_classes = [AllowAny,]
     lookup_field = 'id'
-    authentication_classes=[]
+    # authentication_classes=[]
 
     def get_queryset(self):
         return Listing.objects.filter(listing_type=Listing.LOST)
